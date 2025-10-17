caf59a0e87850c4a41b71efae15dea8d714675e9
gnanda23 <geeta.nanda@gmail.com>
2025-10-17 13:11:02 -0400

    Add files via upload

--- scripts/run_vep.sh
+++ scripts/run_vep.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-perl ~/vep_project/ensembl-vep/vep \
+
+# Set variables
+VEP_PATH="$HOME/vep_project/ensembl-vep/vep"
+CACHE_DIR="/Volumes/Seagate8/.vep"
+FASTA="$CACHE_DIR/homo_sapiens/115_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa.gz"
+INPUT_VCF="/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_input_fixed.vcf"
+OUTPUT_TSV="/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_vep_output.tsv"
+
+# Run VEP
+perl "$VEP_PATH" \
   --cache \
   --offline \
-  --dir_cache "/Volumes/Seagate8/.vep" \
-  --fasta "/Volumes/Seagate8/.vep/homo_sapiens/115_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa.gz" \
+  --dir_cache "$CACHE_DIR" \
+  --fasta "$FASTA" \
   --assembly GRCh38 \
-  --input_file "/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_input_fixed.vcf" \
-  --output_file "/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_vep_output.tsv" \
+  --input_file "$INPUT_VCF" \
+  --output_file "$OUTPUT_TSV" \
   --tab \
   --force_overwrite \
   --everything
