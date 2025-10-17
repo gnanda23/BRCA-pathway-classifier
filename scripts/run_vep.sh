f5530e4c781aca5f2ce299bb0190afd72ec2fea5
gnanda23 <geeta.nanda@gmail.com>
2025-10-17 13:49:11 -0400

    Update run_vep.sh

--- scripts/run_vep.sh
+++ scripts/run_vep.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Set input/output paths
+INPUT_FILE="/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_input_fixed.vcf"
+OUTPUT_FILE="/Volumes/Seagate8/BRCA-pathway-classifier/data/raw/brca_vep_output.tsv"
+
+# Run VEP
 perl ~/vep_project/ensembl-vep/vep \
   --cache \
   --offline \
   --dir_cache "/Volumes/Seagate8/.vep" \
   --fasta "/Volumes/Seagate8/.vep/homo_sapiens/115_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa.gz" \
   --assembly GRCh38 \
-  --input_file "./data/raw/brca_input_fixed.vcf" \
-  --output_file "./data/raw/brca_vep_output.tsv" \
+  --input_file "$INPUT_FILE" \
+  --output_file "$OUTPUT_FILE" \
   --tab \
   --force_overwrite \
   --everything
